efb1df824a4b600f794abb3c7273a24de92c9dff
Tricia Gosney <tricia.gosney@gtech.com>
2020-09-17 12:57:13 -0400

    finished

---
+++ src/components/searchMovies.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import MovieCard from './movieCard';
+
+export default function SearchMovies() {
+
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    //send default state to return an array of the state and a funtion that will update the state of the first parameter
+    const [movies, setMovies] = useState([]);
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=15285ac39ed5d7c3609459ef36e7d269&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+            setMovies(data.results);
+        }
+        catch (err) {
+            console.log(err);
+            setMovies([]);
+        }
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="form">
+                <label className="label" htmlFor="query">
+                    Movie Name
+                    </label>
+                <input
+                    type="text"
+                    name="query"
+                    value={query}
+                    className="input"
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="i.e Jurassic Park"
+                />
+                <button className="button" type="submit">
+                    Search
+                    </button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+
+        </div>
+    )
+
+}
---
+++ src/test.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function () {
+
+    return (
+        <h1>Hello World</h1>
+    )
+
+}
+
+//to create the SearchMovies component
+//form with a class of form
+//label with htmlFor="query" and a class of lable
+//input of type text with a name of "query" and a placeholder
+//button class of button and a type of submit
